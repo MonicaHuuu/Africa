abef2bb18302a1ce6501605db2aa99579a15d6fb
Yile Hu <yh322@duke.edu>
2022-09-27 17:34:44 -0400

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Yile Hu on 9/27/22.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
