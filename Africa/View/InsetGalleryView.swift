2380df476f9677fb8a6efd9dd059ec7562d78b5d
Yile Hu <yh322@duke.edu>
2022-09-29 00:05:21 -0400

    Add a detailed page for each animals, including a picture gallery of it.

---
+++ Africa/View/InsetGalleryView.swift
@@ -0,0 +1,42 @@
+//
+//  InsetGalleryView.swift
+//  Africa
+//
+//  Created by Yile Hu on 9/28/22.
+//
+
+import SwiftUI
+
+struct InsetGalleryView: View {
+    // MARK: - PROPERTIES
+    
+    let animal: Animal
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(alignment: .center, spacing: 15) {
+                ForEach(animal.gallery, id: \.self) { item in
+                    Image(item)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 200)
+                    .cornerRadius(12)
+                }
+            } //: HSTACK
+        } //: SCROLL
+    }
+}
+
+// MARK: -PREVIEW
+
+struct InsetGalleryView_Previews: PreviewProvider {
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
+    
+    static var previews: some View {
+        InsetGalleryView(animal: animals[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
