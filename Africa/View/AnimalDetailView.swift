3eaaacb3568e846a5d6f60f525b58430b94808ba
Yile Hu <yh322@duke.edu>
2022-09-29 10:58:58 -0400

    Add a fun fact part in detail page

--- Africa/View/AnimalDetailView.swift
+++ Africa/View/AnimalDetailView.swift
@@ -47,6 +47,12 @@ struct AnimalDetailView: View {
                 .padding(.horizontal)
                 
                 // FACTS
+                Group {
+                    HeadingView(headingImage: "questionmark.circle", headingText: "Did you know?")
+                    
+                    InsetFactView(animal: animal)
+                }
+                .padding(.horizontal)
                 
                 // DESCRIPTION
                 
---
+++ Africa/View/InsetFactView.swift
@@ -0,0 +1,38 @@
+//
+//  InsetFactView.swift
+//  Africa
+//
+//  Created by Yile Hu on 9/29/22.
+//
+
+import SwiftUI
+
+struct InsetFactView: View {
+    // MARK: - PROPERTIES
+    
+    let animal: Animal
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        GroupBox {
+            TabView {
+                ForEach(animal.fact, id: \.self) { item in
+                    Text(item)
+                } //. LOOP
+            } //. TABS
+            .tabViewStyle(PageTabViewStyle())
+            .frame(minHeight: 148, idealHeight: 168, maxHeight: 180)
+        } //: BOX
+    }
+}
+
+struct InsetFactView_Previews: PreviewProvider {
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
+    
+    static var previews: some View {
+        InsetFactView(animal: animals[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
