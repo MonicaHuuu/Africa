5728ba53278122b00cabe2935ebbbcf8fca4be4c
Yile Hu <yh322@duke.edu>
2022-10-01 14:15:57 -0400

    Create a map view with custom annotation.

--- Africa/App/MapView.swift
+++ Africa/App/MapView.swift
@@ -6,12 +6,82 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    // MARK: - PROPERTIES
+    
+    @State private var region: MKCoordinateRegion = {
+        var mapCoordinates = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599)
+        var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+        var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+        
+        return mapRegion
+    } ()
+    
+    let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
+    
+    // MARK: - BODY
     var body: some View {
-        Text("Map")
+
+        Map(coordinateRegion: $region, annotationItems: locations, annotationContent: { item in
+
+            // MARKER (always static)
+//            MapMarker(coordinate: item.location, tint: .accentColor)
+            
+            // CUSTOM ADVANCED ANNOTATION (it could be interactive)
+            MapAnnotation(coordinate: item.location) {
+                MapAnnotationView(location: item)
+            
+            } //: ANNOTATION
+        }) //: MAP
+        .overlay(
+            HStack(alignment: .center, spacing: 12) {
+                Image("compass")
+                    .resizable()
+                    .scaledToFit()
+                .frame(width: 48, height: 48, alignment: .center)
+            
+                VStack(alignment: .leading, spacing: 3) {
+                    HStack {
+                        Text("Latitude:")
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .foregroundColor(.accentColor)
+                        Spacer()
+                        Text("\(region.center.latitude)")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    } //: HSTACK
+                    
+                    Divider()
+                    
+                    HStack {
+                        Text("Longitude:")
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .foregroundColor(.accentColor)
+                        Spacer()
+                        Text("\(region.center.longitude)")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    } //: HSTACK
+                }
+            } //: HSTACK
+                .padding(.vertical, 12)
+                .padding(.horizontal, 16)
+                .background(
+                    Color.black
+                        .cornerRadius(8)
+                        .opacity(0.6)
+                )
+                .padding()
+            ,alignment: .top
+        )
     }
 }
+
+// MARK: - PREVIEW
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ Africa/Model/LocationModel.swift
@@ -0,0 +1,22 @@
+//
+//  LocationModel.swift
+//  Africa
+//
+//  Created by Yile Hu on 10/1/22.
+//
+
+import Foundation
+import MapKit
+
+struct NationalParkLocation: Codable, Identifiable {
+    var id: String
+    var name: String
+    var image: String
+    var latitude: Double
+    var longitude: Double
+    
+    // Computed Property
+    var location: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
